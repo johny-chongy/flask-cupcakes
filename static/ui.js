5d0a4d2cf026a8294b7bfc39ad3cee185992640f
John Chong <johnchong.99.17@gmail.com>
2023-05-01 18:56:21 -0500

    finish assignment

--- static/ui.js
+++ static/ui.js
@@ -3,29 +3,43 @@ const BASE_URL = "/api/cupcakes";
 const $addCupcakeForm = $("#addCupcake");
 const $cupcakeList = $("#cupcakeList");
 
+startView();
+
+async function startView() {
+  let response = await axios.get(BASE_URL)
+
+  for (let cupcakeResponse of response.data.cupcakes) {
+    appendCupcaketoList(cupcakeResponse);
+  }
+}
+
+
 /**
  * Adding the form data to the database and show it to the page as a list
  */
 async function handleNewCupcake(evt) {
   evt.preventDefault();
 
-  let flavor = $("#flavor").val(); //FIXME: const everything
-  let size = $("#size").val();
-  let rating = $("#rating").val();
-  let image_url = $("#imageUrl").val();
+  const flavor = $("#flavor").val();
+  const size = $("#size").val();
+  const rating = $("#rating").val();
+  const image_url = $("#imageUrl").val();
 
   let cupcake = { flavor, size, rating, image_url };
-
   let response = await axios.post(BASE_URL, cupcake);
-
   let cupcakeResponse = response.data.cupcake;
 
-  //TODO: might be more scalable if separate out
-  //FIXME: get
+  appendCupcaketoList(cupcakeResponse);
+}
 
+
+
+/** Adds cupcake to DOM visually from JSON response value */
+function appendCupcaketoList(cupcakeResponse) {
   $cupcakeList.append(
     `<li><a href="${BASE_URL}/${cupcakeResponse.id}">${cupcakeResponse.flavor}</a></li>`
-  );
-}
+    );
+  }
+
 
 $addCupcakeForm.on("submit", handleNewCupcake);
